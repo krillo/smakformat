5cf8fb4e71769432f4c0a196d0acf3065355ca03
Kristian Erendi <krillo@gmail.com>
2013-03-20 11:24:51 +0100

    fynd images fix

--- single-fynd.php
+++ single-fynd.php
@@ -20,7 +20,6 @@ $fyndObject = getFyndObject(get_the_ID());
 $fyndtype = $fyndObject->fynd_type;
 $fyndtype_txt = $fyndObject->fynd_type_name;
 $fynd_cat_slug = $fyndObject->fynd_cat_slug;
-
 ?>
 
 <!-- #content Starts -->
@@ -50,7 +49,7 @@ $fynd_cat_slug = $fyndObject->fynd_cat_slug;
         <div id="" class="fynd-img">
           <?php
           if (has_post_thumbnail()) {
-            the_post_thumbnail('medium'); 
+          the_post_thumbnail('medium');
           }
           ?> 
         </div>
@@ -79,7 +78,7 @@ $fynd_cat_slug = $fyndObject->fynd_cat_slug;
 
     <div id="sidebar">
       <?php include_once 'snippet_list_fynd.php'; ?>
-      <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar("fyndhyllan")) : endif; ?>    
+      <?php if (!function_exists('dynamic_sidebar') ||!dynamic_sidebar("fyndhyllan")) : endif; ?>    
     </div>
 
 
